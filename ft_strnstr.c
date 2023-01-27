af59e1b4ba8c40b7ef17f40f36de577ff849b9fb
Frattah <frattah02@gmail.com>
2023-01-27 10:59:38 +0100

    strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -17,13 +17,16 @@ char	*ft_strnstr(const char *s1, const char *s2, size_t n)
 	size_t	ln;
 	size_t	i;
 
-	if (s2 == NULL)
-		return ((char *) s1);
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
 	ln = ft_strlen(s2);
+	if (ln == 0)
+		return ((char *) s1);
 	i = 0;
 	while (s1[i] && i < n)
 	{
-		printf("confronto %s && %s\n", s1 + i, s2);
+		if (i + ln > n)
+			return (NULL);
 		if (!ft_strncmp(s1 + i, s2, ln))
 			return ((char *) s1 + i);
 		i++;
